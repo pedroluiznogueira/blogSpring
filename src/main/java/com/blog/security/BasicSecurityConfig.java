ff33e5cb670867ff434b183098c7a785b8e28ee3
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-21 10:37:55 -0300

    Implementando a camada de segurança e a classe BasicSecurityConfig, com @EnableWebSecurity que informa que usaremos o ecossistema de Spring Security

---
+++ src/main/java/com/blog/security/BasicSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.blog.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class BasicSecurityConfig {
+
+
+}

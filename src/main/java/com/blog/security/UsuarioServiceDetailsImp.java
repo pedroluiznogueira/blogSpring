035ae18e0e0747792861e8fcb420b851e83a4781
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-21 12:13:01 -0300

    Para sobreescrever esse método precisamos implementar UserDetailsService, preciso tratar orElseThrow e mapeá-lo com map para retornar um objeto do tipo UsuarioDetailsImp

--- src/main/java/com/blog/security/UsuarioServiceDetailsImp.java
+++ src/main/java/com/blog/security/UsuarioServiceDetailsImp.java
@@ -1,8 +1,32 @@
 package com.blog.security;
 
+import com.blog.model.Usuario;
+import com.blog.repository.UsuarioRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class UsuarioServiceDetailsImp {
+public class UsuarioServiceDetailsImp implements UserDetailsService {
+
+    // preciso persistir o nome de usuario e a senha, verificar ou salvar
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        Optional<Usuario> usuario = usuarioRepository.findByUsuario(username);
+        usuario
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(username + "not found")
+                );
+        return usuario.map(UsuarioDetailsImp::new).get();
+    }
 
 }

0ca1bd4aaa0c6287ac4059f15865c4724864317c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-21 10:40:47 -0300

    Essa classe extende WebSecurityConfigurerAdapter

--- src/main/java/com/blog/security/BasicSecurityConfig.java
+++ src/main/java/com/blog/security/BasicSecurityConfig.java
@@ -1,9 +1,9 @@
 package com.blog.security;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class BasicSecurityConfig {
-
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 }

bea701e7f45c918d579df5f40de8240fce5878c0
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-21 21:53:07 -0300

    Com BCryptPasswordEncoder declaro o tipo de encriptação que vou usar

--- src/main/java/com/blog/service/UsuarioService.java
+++ src/main/java/com/blog/service/UsuarioService.java
@@ -1,7 +1,21 @@
 package com.blog.service;
 
+import com.blog.model.Usuario;
+import com.blog.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        // vamos trabalhar a senha encriptada apartir do BCryptPasswordEncoder da classe BasicSecurityConfig
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    }
 }

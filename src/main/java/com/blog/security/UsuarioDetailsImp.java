221b91007f175811e17554aadf744b50a228f3bc
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-25 14:18:57 -0300

    Fix nos modificadores de acesso da classe UsuarioDetailsImp, sem eles a autenticação do token ao ser passada na Header não é reconhecida

--- src/main/java/com/blog/security/UsuarioDetailsImp.java
+++ src/main/java/com/blog/security/UsuarioDetailsImp.java
@@ -28,12 +28,12 @@ public class UsuarioDetailsImp implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
